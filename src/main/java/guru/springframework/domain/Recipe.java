358776f24851a18f622b043c72c9507ce25437e9
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-08 11:21:13 +0530

    add tetin to add-index-page

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,12 @@
+package guru.springframework.domain;
+
+public class Recipe {
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer serving;
+    private String url;
+    private String direction;
+
+    private Byte[] image;
+}
